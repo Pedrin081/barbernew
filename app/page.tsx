cd41b55a6cf08c47315ae419870afcca61bd1a24
pedrin081 <pedro.vnascimento@outlook.com>
2024-08-07 10:35:53 -0300

    finalização

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-"use client";
-
-import { useState } from "react";
+import { Button } from "./_components/ui/button"
 
 const Home = () => {
-  const [] = useState();
-  return <h1 className="text-red-500">Home page</h1> ;
+  return <Button>teste</Button>
 }
 
-export default Home;
+export default Home
