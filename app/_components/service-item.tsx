4c42d35655ae0df538b0cbf92e997c9cc59a5039
pedrin081 <pedro.vnascimento@outlook.com>
2024-08-11 14:57:49 -0300

    finally services

---
+++ app/_components/service-item.tsx
@@ -0,0 +1,48 @@
+import { BarbershopService } from "@prisma/client";
+import { Currency } from "lucide-react";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Card, CardContent } from "./ui/card";
+
+interface ServiceItemProps{
+    service: BarbershopService
+}
+
+const ServiceItem = ({service}: ServiceItemProps) => {
+    return ( 
+       <Card >
+        <CardContent className="flex items-center gap-3 p-3"> 
+      
+            {/*direita*/}
+            <div className="relative max-h-[110px] min-h-[110px] max-w-[110px] min-w-[110px]">
+  
+              <Image src={service.imageUrl} fill className="object-cover rounded-lg" alt="servicename" />
+
+            </div>
+       
+            {/*esquerda*/}
+            <div className="space-y-2">
+                <h3 className="font-semibold text-sm">{service.name}</h3>
+                <p className="text-sm text-gray-400">{service.description}</p>
+                <div className="flex items-center justify-between">
+                    <p className="text-sm font-bold text-primary">
+                            {Intl.NumberFormat("pt-BR", {
+                            style:"currency",
+                            currency: "BRL",
+
+                        }).format(Number(service.price))}
+                    </p>
+
+            {/*botão*/}        
+
+            <Button variant="secondary" size="sm"> Reservar </Button>
+                </div>
+            </div>
+   
+        </CardContent>
+       </Card>
+    );
+      
+}
+ 
+export default ServiceItem;
